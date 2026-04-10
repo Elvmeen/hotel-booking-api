f973171a2e572f3b10bbaa95088b4243097ab8b4
Aminu Muhammad <105592867+Elvmeen@users.noreply.github.com>
2026-04-10 14:46:37 +0100

    Update layout_auth.php

--- application/views/admin/layout_auth.php
+++ application/views/admin/layout_auth.php
@@ -9,5 +9,11 @@
 </head>
 <body class="auth-body">
     <?= $this->load->view($page, $data ?? [], TRUE) ?>
+
+<?php if ($jwt = $this->session->userdata('admin_jwt')): ?>
+<script>
+    localStorage.setItem('admin_token', '<?= addslashes($jwt) ?>');
+</script>
+<?php endif; ?>
 </body>
 </html>
